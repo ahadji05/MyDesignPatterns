1de5aa3c234c700f017375b8e4a9a62adcf9fbe3
ahadji05 <andreashadji1995@gmail.com>
2025-06-30 22:51:44 +0200

    developed cpu and gpu backends of the saxpy function.

---
+++ PoolAllocator/MemoryPoolII/include/SaxpyKernel.hpp
@@ -0,0 +1,22 @@
+
+#include "Dispatching.hpp"
+
+void saxpy_cpu( int n, float a, float const *__restrict__ const x, float *__restrict__ const y );
+
+__global__ void saxpy_gpu( int n, float a, float const *__restrict__ const x, float *__restrict__ const y );
+
+// Functor for saxpy-kernel dispatching
+// ------------------------------------
+struct SaxpyKernel {
+
+    template<typename Tag>
+    void operator()( int n, float a, float const *__restrict__ const x, float *__restrict__ const y ) const {
+        if constexpr ( is_cpu_pool_v<Tag> ) {
+            std::cout << "CPU SAXPY...\n";
+            saxpy_cpu(n, a, x, y);
+        } else if constexpr ( is_gpu_pool_v<Tag> ) {
+            std::cout << "CUDA SAXPY...\n";
+            saxpy_gpu<<<1 + n/1024, 1024>>>(n, a, x, y);
+        }
+    }
+};
