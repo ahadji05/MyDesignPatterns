dca23df13bdc0135dc1c37e70d46c81111ea2cbf
ahadji05 <andreashadji1995@gmail.com>
2025-07-02 20:29:51 +0200

    removed memory pool from directory RuntimePolymorphismII

--- PoolAllocator/RuntimePolymorphismII/Compute.hpp
+++ PoolAllocator/RuntimePolymorphismII/Compute.hpp
@@ -13,7 +13,7 @@ int run_main_routine();
 /**
  * \brief A basic example.
  */
-void run_example1();
+void run_example();
 
 /*************************************************************************************
  * \brief Compute function that does some calculations using a vector that is allocated by a generic allocator.
@@ -25,9 +25,3 @@ void compute( std::vector<float, AllocatorWrapper<float>> &vec );
  * \brief Example of usage with STL containers: list, map, set, unordered_map, unordered_set.
  */
 void exampleStlContainers();
-
-
-/**
- * \brief A basic example that uses a memory pool for allocations/frees.
- */
-void memory_pool_example();
